50ce54cb7e581f98cdced9b1bc977d9baa513926
Hillary Chemiron <chemiron.hillary@student.moringaschool.com>
2022-08-30 08:33:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function fetchBooks() {
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json)=>renderBooks(json));
   // To pass the tests, don't forget to return your fetch!
   
 }
+fetchBooks();
+console.log (fetchBooks())
 
 function renderBooks(books) {
   const main = document.querySelector('main');
